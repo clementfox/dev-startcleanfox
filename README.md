80d966c88fd556a5757aeef3cab949c23bc7b912
Clément Bataille <clement@misterfox.co>
2018-01-02 12:35:52 +0100

    add readme and sh file

---
+++ README.md
@@ -0,0 +1,5 @@
+# startmanyservices
+
+This script will start different micro services in different terminal windows, to make it work for you: 
+- change the ```pathtoapp``` variable to the folder where you have those services
+- change each corresponding command
---
+++ launchseveral.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+
+pathtoapp='Desktop/cleanfox'
+
+cd $pathtoapp
+mkdir commands
+cd commands
+
+echo "cd $pathtoapp/beetlex && npm start" > beetlex.command
+chmod +x beetlex.command
+open beetlex.command
+
+echo "cd $pathtoapp/mailapi && npm run cleanfox-mode" > mailapi.command
+chmod +x mailapi.command
+open mailapi.command
+
+echo "cd $pathtoapp/pigx && npm start" > pigx.command
+chmod +x pigx.command
+open pigx.command
+
+echo "mongod --dbpath ~/misterfox/db/" > mongo.command
+chmod +x mongo.command
+open mongo.command
+
+echo "cd $pathtoapp/cleanfox-back && npm start" > cleanfox-back.command
+chmod +x cleanfox-back.command
+open cleanfox-back.command
+
+echo "cd $pathtoapp/cleanfox-front && npm run dev" > cleanfox-front.command
+chmod +x cleanfox-front.command
+open cleanfox-front.command
+
+
+#ref https://stackoverflow.com/questions/989349/running-a-command-in-a-new-mac-os-x-terminal-window
